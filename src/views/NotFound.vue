6055f581896d689d6227327476629a080e6adb9e
Jon Tmarz <34428004+Jonedi@users.noreply.github.com>
2023-01-06 09:46:14 -0500

    v1.4

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,7 @@
 <template>
     <a-row>
-        <a-col>
+        <a-col span="24">
+            <h1 class="text-center">Gracias por la visita</h1>
             <div class="justify-center mb-2"><router-link to="/" class="text-center">Ir al home</router-link></div>
             <!-- <img src="../assets/opps_404.jpg" alt="oopppsss 404" class="img-fluid"> -->
         </a-col>
